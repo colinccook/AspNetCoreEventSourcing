2ec5a5be209d8d956886cc73ed268fdf3663de98
Colin Cook <Colin.Cook@flagship-group.co.uk>
2019-02-05 22:32:49 +0000

    put in placeholder pages for all index areas

---
+++ src/ColinCook.RazorPages/Areas/Control/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ColinCook.RazorPages.Areas.Control.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/ColinCook.RazorPages/Areas/Operatives/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ColinCook.RazorPages.Areas.Operatives.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
