81ecb28f7f884b1b422cbd475019877b8c70f1b1
Colin Cook <Colin.Cook@flagship-group.co.uk>
2019-02-05 20:51:32 +0000

    Operatives can now be hired

--- src/ColinCook.RazorPages/Areas/Administration/Pages/Operatives.cshtml.cs
+++ src/ColinCook.RazorPages/Areas/Administration/Pages/Operatives.cshtml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ColinCook.VisitWorkflow.AggregateRoots.Operatives.Command;
 using ColinCook.VisitWorkflow.AggregateRoots.Operatives.Models;
 using ColinCook.VisitWorkflow.AggregateRoots.Operatives.Queries;
+using ColinCook.VisitWorkflow.Identities;
+using EventFlow;
+using EventFlow.Extensions;
 using EventFlow.Queries;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,18 +18,30 @@ namespace ColinCook.RazorPages.Areas.Administration.Pages
     public class OperativesModel : PageModel
     {
         private readonly IQueryProcessor _queryProcessor;
+        private readonly ICommandBus _commandBus;
+
+        [BindProperty] public string Forename { get; set; }
+        [BindProperty] public string Surname { get; set; }
 
         public IReadOnlyCollection<OperativeModel> Operatives { get; set; }
 
-        public OperativesModel(IQueryProcessor queryProcessor)
+        public OperativesModel(IQueryProcessor queryProcessor, ICommandBus commandBus)
         {
             _queryProcessor = queryProcessor;
+            _commandBus = commandBus;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            Operatives = _queryProcessor.Process(
+            Operatives = await _queryProcessor.ProcessAsync(
                 new AllOperativesQuery(), CancellationToken.None);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var result = await _commandBus.PublishAsync(new OperativeHiredCommand(OperativeId.New, Forename, Surname), CancellationToken.None);
+
+            return RedirectToPage();
         }
     }
 }
