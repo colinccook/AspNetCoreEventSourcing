55142ee2abd32d7960e0fe5b4b62f55c42621e4f
Colin Cook <Colin.Cook@flagship-group.co.uk>
2019-02-11 13:39:30 +0000

    bootstrap changes that need to happen

--- src/ColinCook.RazorPages/Areas/Response/Pages/Index.cshtml.cs
+++ src/ColinCook.RazorPages/Areas/Response/Pages/Index.cshtml.cs
@@ -1,16 +1,33 @@
-using System;
+using ColinCook.VisitWorkflow.AggregateRoots.Sites.Models;
+using ColinCook.VisitWorkflow.AggregateRoots.Sites.Queries;
+using ColinCook.VisitWorkflow.Identities;
+using EventFlow;
+using EventFlow.Queries;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ColinCook.RazorPages.Areas.Response.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IQueryProcessor _queryProcessor;
+        private readonly ICommandBus _commandBus;
+
+        public IReadOnlyCollection<SiteModel> Sites { get; set; }
+
+        public IndexModel(IQueryProcessor queryProcessor, ICommandBus commandBus)
         {
+            _queryProcessor = queryProcessor;
+            _commandBus = commandBus;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Sites = await _queryProcessor.ProcessAsync(
+                new AllSitesQuery(), CancellationToken.None);
         }
     }
 }
